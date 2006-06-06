5f1bf4a345d2d076ecaa6105d82e4da15e8d7ef4
Stephan Wahlbrink <sw@wahlbrink.eu>
2006-06-06 14:57:04 +0200

    [RServe] Adapted RServe launch config dialog to changes of databinding API

--- de.walware.statet.r.rserve/src/de/walware/statet/r/rserve/internal/launchconfigs/RServeClientMainTab.java
+++ de.walware.statet.r.rserve/src/de/walware/statet/r/rserve/internal/launchconfigs/RServeClientMainTab.java
@@ -17,20 +17,26 @@ import java.beans.PropertyChangeListener;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
-import org.eclipse.jface.databinding.BeanUpdatableFactory;
-import org.eclipse.jface.databinding.BindSpec;
-import org.eclipse.jface.databinding.DataBinding;
-import org.eclipse.jface.databinding.IDataBindingContext;
-import org.eclipse.jface.databinding.IUpdatableFactory;
-import org.eclipse.jface.databinding.Property;
-import org.eclipse.jface.databinding.swt.SWTUpdatableFactory;
-import org.eclipse.jface.databinding.validator.RegexStringValidator;
+import org.eclipse.jface.internal.databinding.provisional.BindSpec;
+import org.eclipse.jface.internal.databinding.provisional.DataBindingContext;
+import org.eclipse.jface.internal.databinding.provisional.beans.BeanObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.conversion.ConvertString2Integer;
+import org.eclipse.jface.internal.databinding.provisional.description.Property;
+import org.eclipse.jface.internal.databinding.provisional.factories.DefaultBindSupportFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.DefaultBindingFactory;
+import org.eclipse.jface.internal.databinding.provisional.factories.DefaultObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.swt.SWTObservableFactory;
+import org.eclipse.jface.internal.databinding.provisional.validation.RegexStringValidator;
+import org.eclipse.jface.internal.databinding.provisional.viewers.ViewersBindingFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Widget;
 
 import de.walware.eclipsecommon.ui.dialogs.Layouter;
 import de.walware.statet.ui.StatetImages;
@@ -57,14 +63,18 @@ public class RServeClientMainTab extends AbstractLaunchConfigurationTab {
 		setControl(mainComposite);
 		mainComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		
-		SWTUpdatableFactory swtUpdater = new SWTUpdatableFactory();
-		swtUpdater.setUpdateTime(IDataBindingContext.TIME_EARLY);
-		IUpdatableFactory[] updaters = new IUpdatableFactory[] { 
-				new BeanUpdatableFactory(),
-				swtUpdater, 
-//				new ViewersUpdatableFactory(), 
-		};
-		IDataBindingContext dbc = DataBinding.createContext(mainComposite, updaters);
+		final DataBindingContext dbc = new DataBindingContext();
+		dbc.addObservableFactory(new DefaultObservableFactory(dbc));
+		dbc.addObservableFactory(new BeanObservableFactory(dbc, null, new Class[]{Widget.class}));
+		dbc.addObservableFactory(new SWTObservableFactory());
+		dbc.addBindingFactory(new DefaultBindingFactory());
+		dbc.addBindingFactory(new ViewersBindingFactory());
+		dbc.addBindSupportFactory(new DefaultBindSupportFactory());
+		mainComposite.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				dbc.dispose();
+			}
+		});
 		
 		Layouter main = new Layouter(mainComposite, new GridLayout());
 		Layouter layouter = new Layouter(main.addGroup("Connection:"), 2);
@@ -77,14 +87,18 @@ public class RServeClientMainTab extends AbstractLaunchConfigurationTab {
 		fServerPort = layouter.addLabeledTextControl("Server Port:");
 		dbc.bind(fServerPort, 
 				new Property(fConnectionConfig,	ConnectionConfig.PROP_SERVERPORT), 
-		        new BindSpec(null, new RegexStringValidator(
-		                "^[0-9]*$", "^[0-9]+$", "Please enter a number")));		
+		        new BindSpec(null, 
+		        		new ConvertString2Integer(), 
+		        		new RegexStringValidator("^[0-9]*$", "^[0-9]+$", "Please enter a number"), 
+		        		null));		
 
 		fSocketTimeout = layouter.addLabeledTextControl("Socket Timeout (ms):");
 		dbc.bind(fSocketTimeout, 
 				new Property(fConnectionConfig, ConnectionConfig.PROP_SOCKETTIMEOUT), 
-		        new BindSpec(null, new RegexStringValidator(
-		                "^[0-9]*$", "^[0-9]+$", "Please enter a number")));
+		        new BindSpec(null, 
+		        		new ConvertString2Integer(),
+		        		new RegexStringValidator("^[0-9]*$", "^[0-9]+$", "Please enter a number"),
+		        		null));
 		
 		fConnectionConfig.addPropertyChangeListener(new PropertyChangeListener() {
 			public void propertyChange(PropertyChangeEvent evt) {
