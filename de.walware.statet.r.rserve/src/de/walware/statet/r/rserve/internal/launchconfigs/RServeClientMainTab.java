c7ef4bdfb3761c237106b918a8508acffa6e05d4
Stephan Wahlbrink <sw@wahlbrink.eu>
2007-04-06 20:12:05 +0200

    [RServe] Migrated LaunchConfigurationTab for RServeClient to E-3.3 databinding
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.statet.r.rserve/src/de/walware/statet/r/rserve/internal/launchconfigs/RServeClientMainTab.java
+++ de.walware.statet.r.rserve/src/de/walware/statet/r/rserve/internal/launchconfigs/RServeClientMainTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 StatET-Project (www.walware.de/goto/statet).
+ * Copyright (c) 2005-2007 StatET-Project (www.walware.de/goto/statet).
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License 
  * v2.1 or newer, which accompanies this distribution, and is available at
@@ -11,8 +11,15 @@
 
 package de.walware.statet.r.rserve.internal.launchconfigs;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.ParsePosition;
+
+import com.ibm.icu.text.NumberFormat;
+
+import org.eclipse.core.databinding.AggregateValidationStatus;
 import org.eclipse.core.databinding.DataBindingContext;
-import org.eclipse.core.databinding.DefaultBindSpec;
+import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.beans.BeansObservables;
 import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.core.databinding.observable.value.IValueChangeListener;
@@ -26,8 +33,6 @@ import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -35,18 +40,47 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 
 import de.walware.eclipsecommons.ui.dialogs.Layouter;
-import de.walware.eclipsecommons.ui.util.UIAccess;
 
 import de.walware.statet.ui.StatetImages;
 
 
+/**
+ * Tab to configure the connection to the RServe-server.
+ */
 public class RServeClientMainTab extends AbstractLaunchConfigurationTab {
 
+
+	private static class PortValidator implements IValidator {
+		
+		private NumberFormat fFormatter;
+
+		public PortValidator() {
+			NumberFormat fFormatter = NumberFormat.getIntegerInstance();
+			fFormatter.setParseIntegerOnly(true);
+		}
+		public IStatus validate(Object value) {
+			String s = ((String) value).trim();
+			ParsePosition result = new ParsePosition(0);
+			int n = fFormatter.parse(s, result).intValue();
+			if (result.getIndex() == s.length() && result.getErrorIndex() < 0) {
+				if (n >= 0 && n <= 65535) {
+					return Status.OK_STATUS;
+				}
+				return ValidationStatus.error("The valid port range is 0-65535.");
+			}
+			return ValidationStatus.error("Please enter a integer specifing the port");
+		}
+	}
+	
 	
 	private ConnectionConfig fConnectionConfig;
 	
 	private Text fServerAddress;
 	private Text fServerPort;
+	
+	private DataBindingContext fDbc;
+	private AggregateValidationStatus fAggregateStatus;
+	private IStatus fCurrentStatus;
 	
 	
 	public RServeClientMainTab() {
@@ -54,57 +88,59 @@ public class RServeClientMainTab extends AbstractLaunchConfigurationTab {
 		fConnectionConfig = new ConnectionConfig();
 	}
 	
+	public String getName() {
+		
+		return "&Main";
+	}
+	
+	public Image getImage() {
+		
+		return StatetImages.getDefault().getImage(StatetImages.IMG_LAUNCHCONFIG_MAIN);
+	}
+
 	public void createControl(Composite parent) {
 		
 		Composite mainComposite = new Composite(parent, SWT.NONE);
 		setControl(mainComposite);
 		mainComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		
-		Realm realm = Realm.getDefault();
-		if (realm == null) {
-			realm = SWTObservables.getRealm(UIAccess.getDisplay());
-			Realm.setDefault(realm);
-		}
-		final DataBindingContext dbc = new DataBindingContext(realm);
-		mainComposite.addDisposeListener(new DisposeListener() {
-			public void widgetDisposed(DisposeEvent e) {
-				dbc.dispose();
-			}
-		});
-		
 		Layouter main = new Layouter(mainComposite, new GridLayout());
 		Layouter layouter = new Layouter(main.addGroup("Connection:"), 2);
 
 		fServerAddress = layouter.addLabeledTextControl("Server Address:");
-		dbc.bindValue(SWTObservables.observeText(fServerAddress, SWT.Modify), 
+		fServerPort = layouter.addLabeledTextControl("Server Port:");
+
+		initBindings();
+
+		fConnectionConfig.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+				updateLaunchConfigurationDialog();
+			}
+		});
+	}
+
+	private void initBindings() {
+
+		Realm realm = Realm.getDefault();
+		fDbc = new DataBindingContext(realm);
+
+		fDbc.bindValue(SWTObservables.observeText(fServerAddress, SWT.Modify), 
 				BeansObservables.observeValue(fConnectionConfig, ConnectionConfig.PROP_SERVERADDRESS), 
+				null, null);
+
+		fDbc.bindValue(SWTObservables.observeText(fServerPort, SWT.Modify), 
+				BeansObservables.observeValue(fConnectionConfig, ConnectionConfig.PROP_SERVERPORT),
+				new UpdateValueStrategy(UpdateValueStrategy.POLICY_UPDATE)
+						.setAfterGetValidator(new PortValidator()),
 				null);
 		
-		fServerPort = layouter.addLabeledTextControl("Server Port:");
-		dbc.bindValue(SWTObservables.observeText(fServerPort, SWT.Modify), 
-				BeansObservables.observeValue(fConnectionConfig, ConnectionConfig.PROP_SERVERPORT),
-				new DefaultBindSpec()
-						.setTargetValidator(new IValidator() {
-							public IStatus validate(Object value) {
-								try {
-									int n = Integer.parseInt((String) value);
-									if (n < 0 || n > 65535) {
-										return ValidationStatus.error("The valid port range is 0-65535.");
-									}
-									return Status.OK_STATUS;
-								} catch (Throwable t) {
-									return ValidationStatus.error("Please enter a number specifing the port");
-								}
-							}
-						})
-//						.setTargetUpdatePolicy(DataBindingContext.TIME_EARLY)
-				);
-
-		dbc.getValidationStatus().addValueChangeListener(new IValueChangeListener() {
+		fAggregateStatus = new AggregateValidationStatus(fDbc.getBindings(),
+				AggregateValidationStatus.MAX_SEVERITY);
+		fAggregateStatus.addValueChangeListener(new IValueChangeListener() {
 			public void handleValueChange(ValueChangeEvent event) {
-				IStatus status = (IStatus) event.getObservableValue().getValue();
-				if (!status.isOK()) {
-					setErrorMessage(status.getMessage());
+				fCurrentStatus = (IStatus) event.diff.getNewValue();
+				if (!fCurrentStatus.isOK()) {
+					setErrorMessage(fCurrentStatus.getMessage());
 				}
 				else {
 					setErrorMessage(null);
@@ -113,7 +149,23 @@ public class RServeClientMainTab extends AbstractLaunchConfigurationTab {
 			}
 		});
 	}
-
+	
+	@Override
+	public void dispose() {
+		
+		super.dispose();
+		
+		if (fAggregateStatus != null) {
+			fAggregateStatus.dispose();
+			fAggregateStatus = null;
+		}
+		if (fDbc != null) {
+			fDbc.dispose();
+			fDbc = null;
+		}
+	}
+	
+	
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
 		
 		ConnectionConfig.writeDefaultsTo(configuration);
@@ -129,15 +181,16 @@ public class RServeClientMainTab extends AbstractLaunchConfigurationTab {
 		fConnectionConfig.writeTo(configuration);
 	}
 
-	public String getName() {
-		
-		return "&Main";
-	}
-	
-	public Image getImage() {
-		
-		return StatetImages.getDefault().getImage(StatetImages.IMG_LAUNCHCONFIG_MAIN);
-	}
 
+	@Override
+	public boolean isValid(ILaunchConfiguration launchConfig) {
+		
+		return (fCurrentStatus == null || fCurrentStatus.isOK());
+	}
 	
+	@Override
+	public boolean canSave() {
+
+		return (fCurrentStatus != null && fCurrentStatus.isOK());
+	}
 }
