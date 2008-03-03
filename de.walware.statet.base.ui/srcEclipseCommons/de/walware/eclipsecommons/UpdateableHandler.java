5db23d9050003a8684452d7db412db11f63e5176
Stephan Wahlbrink <sw@wahlbrink.eu>
2008-03-03 13:32:34 +0100

    Workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=210862 in E-3.3
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.statet.base.ui/srcEclipseCommons/de/walware/eclipsecommons/UpdateableHandler.java
+++ de.walware.statet.base.ui/srcEclipseCommons/de/walware/eclipsecommons/UpdateableHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 WalWare/StatET-Project (www.walware.de/goto/statet).
+ * Copyright (c) 2007-2008 WalWare/StatET-Project (www.walware.de/goto/statet).
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,6 +13,9 @@ package de.walware.eclipsecommons;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.HandlerEvent;
+import org.eclipse.swt.widgets.Display;
+
+import de.walware.eclipsecommons.ui.util.UIAccess;
 
 
 /**
@@ -37,7 +40,18 @@ public abstract class UpdateableHandler extends AbstractHandler {
 	protected void setEnabled(final boolean enabled) {
 		if (fIsEnabled != enabled) {
 			fIsEnabled = enabled;
-			fireHandlerChanged(new HandlerEvent(this, true, false));
+			final Display display = UIAccess.getDisplay();
+			final Runnable update = new Runnable() {
+				public void run() {
+					fireHandlerChanged(new HandlerEvent(UpdateableHandler.this, true, false));
+				}
+			};
+			if (display.getThread() == Thread.currentThread()) {
+				update.run();
+			}
+			else {
+				display.asyncExec(update);
+			}
 		}
 	}
 	
