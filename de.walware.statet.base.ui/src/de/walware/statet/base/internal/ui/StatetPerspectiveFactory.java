6b454643e2260e8d71d6a99a1da23dd5d02f19c6
Stephan Wahlbrink <sw@wahlbrink.eu>
2009-05-02 12:11:17 +0200

    [Perspective] Enhanced perspective   - Added new views   - Show more views by default
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.statet.base.ui/src/de/walware/statet/base/internal/ui/StatetPerspectiveFactory.java
+++ de.walware.statet.base.ui/src/de/walware/statet/base/internal/ui/StatetPerspectiveFactory.java
@@ -26,6 +26,9 @@ public class StatetPerspectiveFactory implements IPerspectiveFactory {
 	private static final String ID_SEARCH_VIEW = "org.eclipse.search.ui.views.SearchView"; // NewSearchUI.SEARCH_VIEW_ID)  //$NON-NLS-1$
 	private static final String ID_CONSOLE_VIEW = "org.eclipse.ui.console.ConsoleView"; // IConsoleConstants.ID_CONSOLE_VIEW //$NON-NLS-1$
 	
+	private static final String ID_NICO_CMDHISTORY_VIEW = "de.walware.statet.nico.views.HistoryView"; //$NON-NLS-1$
+	private static final String ID_NICO_OBJECTBROWSER_VIEW = "de.walware.statet.nico.views.ObjectBrowser"; //$NON-NLS-1$
+	
 	
 	/**
 	 * Constructs a new Default layout engine.
@@ -34,6 +37,7 @@ public class StatetPerspectiveFactory implements IPerspectiveFactory {
 		super();
 	}
 	
+	
 	public void createInitialLayout(final IPageLayout layout) {
 		final String editorArea = layout.getEditorArea();
 		
@@ -41,17 +45,21 @@ public class StatetPerspectiveFactory implements IPerspectiveFactory {
 		leftFolder.addView(ProjectExplorer.VIEW_ID);
 		leftFolder.addPlaceholder(IPageLayout.ID_RES_NAV);
 		
-		final IFolderLayout outputfolder = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.75f, editorArea); //$NON-NLS-1$
-		outputfolder.addView(IPageLayout.ID_TASK_LIST);
+		final IFolderLayout outputfolder = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.70f, editorArea); //$NON-NLS-1$
 		outputfolder.addView(ID_CONSOLE_VIEW);
+		outputfolder.addView(IPageLayout.ID_TASK_LIST);
 		outputfolder.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
 		outputfolder.addPlaceholder(ID_SEARCH_VIEW);
 		outputfolder.addPlaceholder(IPageLayout.ID_BOOKMARKS);
 		outputfolder.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
 		
-		final IFolderLayout editorAddonFolder = layout.createFolder("editor-additions", IPageLayout.RIGHT, 0.75f, editorArea); //$NON-NLS-1$
-		editorAddonFolder.addView(IPageLayout.ID_OUTLINE);
-		editorAddonFolder.addPlaceholder(TemplatesView.ID);
+		final IFolderLayout editorAddFolder = layout.createFolder("editor-additions", IPageLayout.RIGHT, 0.75f, editorArea); //$NON-NLS-1$
+		editorAddFolder.addView(IPageLayout.ID_OUTLINE);
+		editorAddFolder.addPlaceholder(TemplatesView.ID);
+		
+		final IFolderLayout consoleAddFolder = layout.createFolder("console-additions", IPageLayout.BOTTOM, 0.60f, "left"); //$NON-NLS-1$ //$NON-NLS-2$
+		consoleAddFolder.addView(ID_NICO_OBJECTBROWSER_VIEW); 
+		consoleAddFolder.addView(ID_NICO_CMDHISTORY_VIEW); 
 		
 //		layout.createPlaceholderFolder("console-additions", IPageLayout.BOTTOM, 0.80f, "left"); //$NON-NLS-1$
 		
@@ -61,15 +69,17 @@ public class StatetPerspectiveFactory implements IPerspectiveFactory {
 		// views - search
 		layout.addShowViewShortcut(ID_SEARCH_VIEW);
 		
-		// views - debugging
-		layout.addShowViewShortcut(ID_CONSOLE_VIEW);
-		
 		// views - standard workbench
 		layout.addShowViewShortcut(ProjectExplorer.VIEW_ID);
 		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
 		layout.addShowViewShortcut(TemplatesView.ID);
 		layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
 		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+		
+		// views - debugging
+		layout.addShowViewShortcut(ID_CONSOLE_VIEW);
+		layout.addShowViewShortcut(ID_NICO_OBJECTBROWSER_VIEW); 
+		layout.addShowViewShortcut(ID_NICO_CMDHISTORY_VIEW); 
 		
 		// new actions
 		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
