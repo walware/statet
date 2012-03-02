558c03da0f58d254dc1b83ab4326d100bab70a0f
Stephan Wahlbrink <sw@wahlbrink.eu>
2012-03-02 16:55:27 +0100

    [R-Help] Changed http content type encoding for HTML content to UTF-8
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.statet.r.core/src/de/walware/statet/r/core/rhelp/RHelpServlet.java
+++ de.walware.statet.r.core/src/de/walware/statet/r/core/rhelp/RHelpServlet.java
@@ -356,11 +356,11 @@ public abstract class RHelpServlet extends HttpServlet {
 					final byte[] buffer = new byte[1024];
 					
 					if (file.getName().endsWith(".html")) { //$NON-NLS-1$
-						resp.setContentType("text/html;charset=US-ASCII"); //$NON-NLS-1$
+						resp.setContentType("text/html; charset=UTF-8"); //$NON-NLS-1$
 						resp.setHeader("Cache-Control", "max-age=600, must-revalidate"); //$NON-NLS-1$ //$NON-NLS-2$
 					}
 					else if (file.getName().indexOf(".") < 0) { //$NON-NLS-1$
-						resp.setContentType("text/plain;charset=US-ASCII"); //$NON-NLS-1$
+						resp.setContentType("text/plain; charset=US-ASCII"); //$NON-NLS-1$
 					}
 					final ServletOutputStream outputStream = resp.getOutputStream();
 					
