4110b33be4e985295a9b60b6b81973127e1937c0
Stephan Wahlbrink <sw@wahlbrink.eu>
2016-02-19 21:48:22 +0100

    Updated copyright for 2016
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.statet.r.core-tests/src/de/walware/statet/r/core/rsource/ast/RArgumentMatchingTest.java
+++ de.walware.statet.r.core-tests/src/de/walware/statet/r/core/rsource/ast/RArgumentMatchingTest.java
@@ -1,3 +1,14 @@
+/*=============================================================================#
+ # Copyright (c) 2008-2016 Stephan Wahlbrink (WalWare.de) and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+ # 
+ # Contributors:
+ #     Stephan Wahlbrink - initial API and implementation
+ #=============================================================================*/
+
 package de.walware.statet.r.core.rsource.ast;
 
 import org.junit.Assert;
